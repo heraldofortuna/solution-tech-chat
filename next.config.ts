264d11b4ba8f2a6a991550db75b47096a5f85edc
Heraldo Fortuna <heraldofortuna@gmail.com>
2025-07-24 22:35:24 -0500

    initial setup and add depedencies

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [],
+  },
+}
+
+export default nextConfig
